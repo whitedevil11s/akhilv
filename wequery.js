c0372a5fc3e4d1b42757cbd028c2176d7bd5753b
whitedevil11s <akhilvvast@gmail.com>
2020-12-26 23:19:39 +0530

    new

---
+++ wequery.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $(document).click(function(){
+       // $("#invsbl").css({'color':'#ffff'})
+       $("#in").fadeToggle(6000)
+       
+    })
+
+   
+
+    $("#web").click(function(){
+      alert(" Do you want a websight")
+     
+      
+   })
+
+   $("#hack").click(function(){
+      alert("Do you want to hack system")
+     
+      
+   })
+
+   $("#cloud").click(function(){
+      alert(" Do you want cloud storage")
+     
+      
+   })
+
+   $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+})
+
+
+
+
+  
+
